7380d6eafdae202274da83d7a3111eb429f43893
ianmeirellespf <ianmeirelles@poli.ufrj.br>
2022-06-28 23:25:11 -0300

    abstract

--- app/cliente/model.py
+++ app/cliente/model.py
@@ -6,16 +6,17 @@ from flask import Blueprint
 cliente_api = Blueprint("cliente_api", __name__)
 
 class cliente(BaseModel):
+
     __tablename__ = "cliente"
 
-    id = db.Column(db.Integer, Primary_Key= True)
+    id = db.column(db.Integer , Primary_Key= True)
     nome = db.column(db.String(100))
     cpf= db.column(db.String(11))
     idade = db.column(db.Integer)
-    pedidos = db.relationship("entrega", backref="cliente")
+    entrega = db.relationship("entrega", backref="cliente")
     supermercado = db.column(db.Integer)
-    suportes = db.relationship("suporte", backref="cliente")
+    suporte = db.relationship("suporte", backref="cliente")
 
-    pass
+    
 
 
--- app/model.py
+++ app/model.py
@@ -2,7 +2,8 @@ from  .extensions import db
 
 
 class BaseModel(db.Model):
-     
+    __abstract__= True
+
     @staticmethod
     def delete(obj):
         db.session.delete(obj)
